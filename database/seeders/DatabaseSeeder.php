04d19a04c3d09a6da7eb6c6be02324684e08d83f
Dekaver <Alwiwahyudi8@gmail.com>
2023-06-06 10:39:51 +0800

    company user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Company;
-use App\Models\UserCompany;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
@@ -22,6 +21,11 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $company1 = Company::factory()->create();
+        $company2 = Company::factory()->create();
+        $company3 = Company::factory()->create();
+
 
         Permission::create(['name' => 'MANAJEMEN_USER']);
 
@@ -43,34 +47,16 @@ class DatabaseSeeder extends Seeder
         $user_a = \App\Models\User::factory()->create([
             'name' => 'admin_a',
             'email' => 'admin_a@gmail.com',
+            'company_id' => $company1->id
         ]);
         $user_a->assignRole($role_a);
 
         $user_b = \App\Models\User::factory()->create([
             'name' => 'admin_b',
             'email' => 'admin_b@gmail.com',
+            'company_id' => $company2->id
         ]);
         $user_b->assignRole($role_b);
-
-        $company1 = Company::factory()->create();
-        $company2 = Company::factory()->create();
-        $company3 = Company::factory()->create();
-
-        $company_all = Company::all();
-        foreach ($company_all as $key => $item) {
-            UserCompany::create([
-                "user_id" => $user->id,
-                "company_id" => $item->id,
-            ]);
-        }
-        UserCompany::create([
-            "user_id" => $user_a->id,
-            "company_id" => $company1->id,
-        ]);
-        UserCompany::create([
-            "user_id" => $user_b->id,
-            "company_id" => $company2->id,
-        ]);
 
 
     }
