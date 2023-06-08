fbfb6fd28c90ec9a734bd54aed2ec77a6cc87a2f
Dekaver <Alwiwahyudi8@gmail.com>
2023-06-08 14:07:24 +0800

    migration

--- database/migrations/2023_06_06_014229_create_tire_compounds_table.php
+++ database/migrations/2023_06_06_014229_create_tire_compounds_table.php
@@ -4,22 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('tire_compounds', function (Blueprint $table) {
-            $table->engine = "MyISAM";
-            $table->integer("id")->unsigned();
-            $table->integer("company_id")->unsigned();
+            $table->id();
+            $table->foreignId("company_id")->constrained("companies");
             $table->string("compound");
             $table->timestamps();
-            $table->primary(array("id", "company_id"));
         });
-        DB::statement('ALTER TABLE tire_compounds MODIFY id INTEGER NOT NULL AUTO_INCREMENT');
     }
 
     /**
