1346f1bde0e79690ce2bfc722a0cdde19cd97ab2
Dekaver <Alwiwahyudi8@gmail.com>
2023-06-19 15:36:24 +0800

    dbseed hide

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -39,8 +39,8 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $company1 = Company::factory()->create();
-        $company2 = Company::factory()->create();
-        $company3 = Company::factory()->create();
+        // $company2 = Company::factory()->create();
+        // $company3 = Company::factory()->create();
 
 
         Permission::insert(
@@ -94,24 +94,24 @@ class DatabaseSeeder extends Seeder
         ]);
         $user->assignRole($role);
 
-        $user_a = User::factory()->create([
-            'name' => 'admin_a',
-            'email' => 'admin_a@gmail.com',
-            'company_id' => $company1->id
-        ]);
-        $user_a->assignRole($role_a);
-        $user_a->syncPermissions("TIRE_MANUFACTURE");
+        // $user_a = User::factory()->create([
+        //     'name' => 'admin_a',
+        //     'email' => 'admin_a@gmail.com',
+        //     'company_id' => $company1->id
+        // ]);
+        // $user_a->assignRole($role_a);
+        // $user_a->syncPermissions("TIRE_MANUFACTURE");
 
         $user->userSite()->create([
             "site_id" => $site->id,
         ]);
 
-        $user_b = User::factory()->create([
-            'name' => 'admin_b',
-            'email' => 'admin_b@gmail.com',
-            'company_id' => $company2->id
-        ]);
-        $user_b->assignRole($role_a);
+        // $user_b = User::factory()->create([
+        //     'name' => 'admin_b',
+        //     'email' => 'admin_b@gmail.com',
+        //     'company_id' => $company2->id
+        // ]);
+        // $user_b->assignRole($role_a);
 
         TireSupplier::create([
             "name" => "AKM",
@@ -145,42 +145,33 @@ class DatabaseSeeder extends Seeder
 
 
         TireStatus::create([
-            'company_id' => 1,
             "status" => 'NEW'
         ]);
         TireStatus::create([
-            'company_id' => 1,
             "status" => 'SPARE'
         ]);
         TireStatus::create([
-            'company_id' => 1,
             "status" => 'REPAIR'
         ]);
         TireStatus::create([
-            'company_id' => 1,
             "status" => 'RETREAD'
         ]);
         TireStatus::create([
-            'company_id' => 1,
             "status" => 'SCRAP'
         ]);
         TireStatus::create([
-            'company_id' => 1,
             "status" => 'RUNNING'
         ]);
 
         UnitStatus::create([
-            'company_id' => 1,
             "status_code" => "RFU",
             "description" => "Running"
         ]);
         UnitStatus::create([
-            'company_id' => 1,
             "status_code" => "STBY",
             "description" => "Running"
         ]);
         UnitStatus::create([
-            'company_id' => 1,
             "status_code" => "BD",
             "description" => "Running"
         ]);
@@ -197,29 +188,29 @@ class DatabaseSeeder extends Seeder
             "axle_8_tire" => 0,
         ]);
 
-        UnitModel::create([
-            'company_id' => 1,
-            "tire_size_id" => 1,
-            "brand" => "Scania",
-            "model" => "P360 LA 6X6",
-            "type" => "PRIME MOVER",
-            "tire_qty" => 10,
-            "axle_2_tire" => 1,
-            "axle_4_tire" => 2,
-            "axle_8_tire" => 0,
-        ]);
+        // UnitModel::create([
+        //     'company_id' => 1,
+        //     "tire_size_id" => 1,
+        //     "brand" => "Scania",
+        //     "model" => "P360 LA 6X6",
+        //     "type" => "PRIME MOVER",
+        //     "tire_qty" => 10,
+        //     "axle_2_tire" => 1,
+        //     "axle_4_tire" => 2,
+        //     "axle_8_tire" => 0,
+        // ]);
 
-        UnitModel::create([
-            'company_id' => 1,
-            "tire_size_id" => 2,
-            "brand" => "Scania",
-            "model" => "P360 LA 6X6",
-            "type" => "PRIME MOVER",
-            "tire_qty" => 10,
-            "axle_2_tire" => 1,
-            "axle_4_tire" => 2,
-            "axle_8_tire" => 0,
-        ]);
+        // UnitModel::create([
+        //     'company_id' => 1,
+        //     "tire_size_id" => 2,
+        //     "brand" => "Scania",
+        //     "model" => "P360 LA 6X6",
+        //     "type" => "PRIME MOVER",
+        //     "tire_qty" => 10,
+        //     "axle_2_tire" => 1,
+        //     "axle_4_tire" => 2,
+        //     "axle_8_tire" => 0,
+        // ]);
 
         TireDamage::insert(
             array(
@@ -259,60 +250,60 @@ class DatabaseSeeder extends Seeder
                 array('id' => '33', 'company_id' => 1, 'damage' => 'STONE DRILLING', 'cause' => 'Operational', 'rating' => '-', 'created_at' => '2022-08-31 08:59:53', 'updated_at' => '2022-08-31 08:59:53')
             )
         );
-        TireMaster::factory()->count(1000)->create();
-        Unit::factory()->count(100)->create();
+        // TireMaster::factory()->count(1000)->create();
+        // Unit::factory()->count(100)->create();
 
-        $unit = Unit::limit(10)->get();
-        foreach ($unit as $key => $value) {
-            for ($i = 0; $i < $value->unit_model->tire_qty; $i++) {
-                $tire = TireMaster::where("tire_status_id", 1)->inRandomOrder()->first();
-                $tirerunning = TireRunning::create([
-                    "unit_id" => $value->id,
-                    "tire_id" => $tire->id,
-                    "site_id" => 1,
-                    "company_id" => 1,
-                    "position" => $i + 1,
-                ]);
-                TireMovement::create([
-                    "tire_running_id" => $tirerunning->id,
-                    "hm" => 0,
-                    "km" => 0,
-                    "unit_lifetime_hm" => $value->hm,
-                    "unit_lifetime_km" => $value->km,
-                    "tire_lifetime_hm" => $tire->lifetime_hm,
-                    "tire_lifetime_km" => $tire->lifetime_km,
-                    "rtd" => $tire->rtd,
-                    "start_date" => \Carbon\Carbon::now(),
-                    "end_date" => \Carbon\Carbon::now(),
-                    "pic" => fake()->name(),
-                    "pic_man_power" => fake()->firstNameMale(),
-                    "desc" => "install"
-                ]);
-                $tire->tire_status_id = 6;
-                $tire->save();
+        // $unit = Unit::limit(10)->get();
+        // foreach ($unit as $key => $value) {
+        //     for ($i = 0; $i < $value->unit_model->tire_qty; $i++) {
+        //         $tire = TireMaster::where("tire_status_id", 1)->inRandomOrder()->first();
+        //         $tirerunning = TireRunning::create([
+        //             "unit_id" => $value->id,
+        //             "tire_id" => $tire->id,
+        //             "site_id" => 1,
+        //             "company_id" => 1,
+        //             "position" => $i + 1,
+        //         ]);
+        //         TireMovement::create([
+        //             "tire_running_id" => $tirerunning->id,
+        //             "hm" => 0,
+        //             "km" => 0,
+        //             "unit_lifetime_hm" => $value->hm,
+        //             "unit_lifetime_km" => $value->km,
+        //             "tire_lifetime_hm" => $tire->lifetime_hm,
+        //             "tire_lifetime_km" => $tire->lifetime_km,
+        //             "rtd" => $tire->rtd,
+        //             "start_date" => \Carbon\Carbon::now(),
+        //             "end_date" => \Carbon\Carbon::now(),
+        //             "pic" => fake()->name(),
+        //             "pic_man_power" => fake()->firstNameMale(),
+        //             "desc" => "install"
+        //         ]);
+        //         $tire->tire_status_id = 6;
+        //         $tire->save();
 
-                HistoryTireMovement::create(
-                    [
-                        "user_id" => 1,
-                        "company_id" => 1,
-                        "site_id" => 1,
-                        "unit" => $value->unit_number,
-                        "tire" => $tire->serial_number,
-                        "position" => $i + 1,
-                        "status" => "RUNNING",
-                        "km_unit_install" => $value->km,
-                        "hm_unit_install" => $value->hm,
-                        "pic" => fake()->firstNameMale(),
-                        "pic_man_power" => fake()->firstNameMale(),
-                        "des" => "Install",
-                        "km_tire_install" => $tire->lifetime_km,
-                        "hm_tire_install" => $tire->lifetime_km,
-                        "start_date" => \Carbon\Carbon::now(),
-                        "end_date" => \Carbon\Carbon::now()
-                    ]
-                );
-            }
-        }
+        //         HistoryTireMovement::create(
+        //             [
+        //                 "user_id" => 1,
+        //                 "company_id" => 1,
+        //                 "site_id" => 1,
+        //                 "unit" => $value->unit_number,
+        //                 "tire" => $tire->serial_number,
+        //                 "position" => $i + 1,
+        //                 "status" => "RUNNING",
+        //                 "km_unit_install" => $value->km,
+        //                 "hm_unit_install" => $value->hm,
+        //                 "pic" => fake()->firstNameMale(),
+        //                 "pic_man_power" => fake()->firstNameMale(),
+        //                 "des" => "Install",
+        //                 "km_tire_install" => $tire->lifetime_km,
+        //                 "hm_tire_install" => $tire->lifetime_km,
+        //                 "start_date" => \Carbon\Carbon::now(),
+        //                 "end_date" => \Carbon\Carbon::now()
+        //             ]
+        //         );
+        //     }
+        // }
 
     }
 }
